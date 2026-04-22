02a4cbd57c7cb7b7fc90b0a4cc26cafd6cbf83d4
epain09 <huang.elaine.ca@gmail.com>
2026-04-21 23:36:44 -0700

    count up total tutee #

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,5 +1,7 @@
 import * as d3 from 'd3';
 import {useEffect, useRef, useState} from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import CountUp from './ui/CountUp'
 export default function WorldMap() {
     const mapref = useRef();
     const pointref = useRef();
@@ -17,6 +19,18 @@ export default function WorldMap() {
                 "ARE": 1,
                 "LTU": 1,
             };
+
+    let sm = 0;
+    Object.keys(data).forEach(function(key, index) {
+        sm += data[key];
+    })
+    console.log(sm);
+
+    // let tp = [];
+    // for (let i=1; i<=sm; i++) {
+    //     tp.push(`Number of tutees: ${i}`);
+    //     tp.push(50);
+    // }
 
     useEffect(() => {
         const width = 960;
@@ -68,8 +82,28 @@ export default function WorldMap() {
     }
     )
     return (
-        <div>
-            <svg className = "w-[1000px] h-[600px]" id = "map" ref={mapref}>
+        <div className = "w-full flex flex-col justify-center items-center">
+            {/* <TypeAnimation
+                sequence = {tp}
+                wrapper="span"
+                speed={10}
+                style={{ fontSize: '2em', display: 'inline-block', 'color': 'black' }}
+                repeat={Infinity}
+                omitDeletionAnimation={true}
+            /> */}
+            <div className = "text-black text-xl w-full flex justify-center items-center">
+                <p className = "mr-2">Number of tutees around the world:</p>
+                <CountUp
+                from={1}
+                to={sm}
+                separator=","
+                direction="up"
+                duration={1}
+                className="count-up-text"
+                delay={0}
+                />
+            </div>
+            <svg className = "h-auto" id = "map" ref={mapref}>
                 <g ref = {gref}></g>
             </svg>
             <div className = {"tooltip absolute bg-white text-black p-2 rounded-xl " + pos} ref = {pointref}></div>
